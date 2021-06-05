710daaffa3cf723343052b54df20612b5f85627b
Roshan <roshan2008web@gmail.com>
2021-06-05 12:58:09 +1200

    create logs folder if doesn't exist in the root

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 from utilities import config_reader
 
@@ -10,10 +11,17 @@ class Logger:
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(file_level)
 
+        # create logs folder if doesn't exist in the root
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+
         # get the name of the log file form the configurations file
         log_file_name = config_reader.read(section="settings", key="log_file_name")
 
         log_line_format = logging.Formatter('%(asctime)s - %(filename)s:[%(lineno)d] [%(levelname)s] %(message)s')
+
+
+
 
         # file handler is for the log file stored in 'logs' folder
         file_handler = logging.FileHandler(filename=f"logs/{log_file_name}", mode="a")
