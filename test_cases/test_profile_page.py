a20afe0f1ba9543d0afd7b7d644ded6f55ac5568
Roshan Adhikari <roshan2008web@gmail.com>
2021-06-17 20:23:12 +1200

    fix success alert reading issue when multiple success alerts appear

--- test_cases/test_profile_page.py
+++ test_cases/test_profile_page.py
@@ -12,6 +12,17 @@ def navigate_to_page_under_test(request):
     request.cls.driver.get(urls.profile_page)
 
 
+def is_in_change_success_messages(page_obj, text):
+    """checks if the given text in available in a list of success messages received after saving the project.
+
+        :return: True if the text is found in the list of messages
+    """
+    success_messages = page_obj.get_text_from_list_of_elements("lbl_profile_change_success_alert")
+    for text in success_messages:
+        return True
+    return False
+
+
 @pytest.mark.usefixtures("navigate_to_page_under_test")
 class Test_ProfilePage(BaseTest):
     @allure.story(test_cases.get_test_case("test_changing_profile").story)
@@ -20,8 +31,9 @@ class Test_ProfilePage(BaseTest):
     def test_changing_profile(self):
         profile_page = profile.ProfilePage(self.driver)
 
-        random_profile_data = spreadsheet_data_provider.get_random_record(workbook_name="resources/data_sheets/random_data.xlsx",
-                                                                          sheet_name="user_profile_data")
+        random_profile_data = spreadsheet_data_provider.get_random_record(
+            workbook_name="resources/data_sheets/random_data.xlsx",
+            sheet_name="user_profile_data")
         random_name = random_profile_data[2]  # index 2 contains full name
         random_password = random_profile_data[6]  # index 6 contains a random password
         random_color = random.choice(list(ProfileColors)).value
@@ -30,22 +42,22 @@ class Test_ProfilePage(BaseTest):
 
         # test changing only profile color
         profile_page.change_profile(color=random_color)
-        assert "Your avatar color has been changed." in profile_page.get_text("lbl_profile_change_success_alert"), \
+        assert is_in_change_success_messages(profile_page, "Your avatar color has been changed."), \
             "Avatar color change alert did not appear"
 
         # test changing only profile name
         profile_page.change_profile(name=random_name)
-        assert "Your name has been updated." in profile_page.get_text("lbl_profile_change_success_alert"), \
+        assert is_in_change_success_messages(profile_page, "Your name has been updated."), \
             "Name change alert did not appear"
 
         # test changing only profile picture
         profile_page.change_profile(profile_photo=random_profile_photo)
-        assert "Your user image has been changed." in profile_page.get_text("lbl_profile_change_success_alert"), \
+        assert is_in_change_success_messages(profile_page, "Your user image has been changed."), \
             "Profile photo change alert did not appear"
 
         # test changing only profile name
         profile_page.change_profile(current_password=password, new_password=random_password)
-        assert "Your password has been updated." in profile_page.get_text("lbl_profile_change_success_alert"), \
+        assert is_in_change_success_messages(profile_page, "Your password has been updated."), \
             "Password change alert did not appear"
 
         # reset password back to the default password
